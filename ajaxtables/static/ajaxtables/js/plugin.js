769228701dff32677cfc199a255a9ee8b85ee3fe
Lovas Bence <lovas.bence@codehouse.hu>
2013-11-12 21:02:32 +0100

    Add onRealod event (event is triggered when the new DOM is inserted to the table

--- ajaxtables/static/ajaxtables/js/plugin.js
+++ ajaxtables/static/ajaxtables/js/plugin.js
@@ -1,13 +1,11 @@
-$.fn.ajaxTable = function() {
-    var option = arguments[0]
-    
+(function($) {
     function AjaxTable(el, options) {
         this.element = el
         this.url = el.data("ajaxsource") || window.location.href;
-        console.log(this.url)
         this.post = el.data("post")
         this.method = this.post == undefined ? 'get' : 'post'
-        
+        this.onReload = options.onReload;
+
         this.init = function() {
             page_size = $.cookie("page_size") || el.data("pagesize") || 10;
             $.cookie("page_size", page_size);
@@ -26,10 +24,10 @@ $.fn.ajaxTable = function() {
                 success: function (retval) {
                     parent.element.find('tbody').html($(retval).find('tbody > tr'))
                     parent.element.find('tfoot').html($(retval).find('tfoot > tr'))
-
                     $('ul.pagination > li > a').click(function (e) {
                         e.preventDefault();
                     });
+                    parent.onReload();
                 }
             });
             
@@ -44,23 +42,33 @@ $.fn.ajaxTable = function() {
             });
         }
     };
-          
-    this.each(function() {
-        var element = $(this)
-        var data = $(this).data('ajaxTable')
-        allowedMethods = ["reload"]
 
-        if (!data) {
-            data = new AjaxTable(element, arguments)
-            $(this).data('ajaxTable', data);
-        }
-        if (typeof option === 'string') {
-            if ($.inArray(option, allowedMethods) < 0) {
-                throw "Unknown method: " + option;
+    $.fn.ajaxTable = function() {
+        var option = arguments[0],
+            args = arguments,
+            allowedMethods = ["reload"];
+         
+        this.each(function() {
+            var element = $(this)
+            var data = $(this).data('ajaxTable')
+            options = $.extend({}, $.fn.ajaxTable.defaults, typeof option === 'object' && option);
+
+            if (!data) {
+                data = new AjaxTable(element, options)
+                $(this).data('ajaxTable', data);
             }
-            value = data[option]();
-        } else {
-            data.init();
-        }
-    });
-};
+            if (typeof option === 'string') {
+                if ($.inArray(option, allowedMethods) < 0) {
+                    throw "Unknown method: " + option;
+                }
+                value = data[option]();
+            } else {
+                data.init();
+            }
+        });
+    };
+
+    $.fn.ajaxTable.defaults = {
+        onReload: function() { return false;}
+    };
+})(jQuery);
