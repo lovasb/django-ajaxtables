0866c349875f10567799110c3fb72cdea6bfd17a
Lovas Bence <lovas.bence@codehouse.hu>
2014-11-22 19:06:41 +0100

    Remove autoload function, force to explicite javascript calls

--- ajaxtables/templatetags/ajaxtables_tags.py
+++ ajaxtables/templatetags/ajaxtables_tags.py
@@ -5,9 +5,8 @@ from django.template.loader import render_to_string
 register = template.Library()
 
 @register.simple_tag(takes_context=False)
-def ajaxtables_imports(import_cookies_js=True, import_autoload=True, template_name='ajaxtables/imports.html'):
+def ajaxtables_imports(import_cookies_js=True, template_name='ajaxtables/imports.html'):
     """ 
     Renders the imports file.
     """
-    return render_to_string(template_name, {'import_cookies_js': import_cookies_js, 
-                                            'import_autoload': import_autoload})
+    return render_to_string(template_name, {'import_cookies_js': import_cookies_js})
